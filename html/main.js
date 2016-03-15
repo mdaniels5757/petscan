5f5ab91f7d014344dbf25a2bfcb27a82c7a6a215
Magnus Manske <magnusmanske@googlemail.com>
2016-03-15 14:50:49 +0000

    interface work

---
+++ html/main.js
@@ -0,0 +1,165 @@
+var params = {} ;
+var default_params = {
+	'show_redirects':'both',
+	'edits[bots]':'both',
+	'edits[anons]':'both',
+	'edits[flagged]':'both',
+	'ns[0]':'1'
+} ;
+
+var max_namespace_id = 0 ;
+var namespaces_selected = [] ;
+var namespaces_loading = false ;
+var last_namespaces = {} ;
+var last_namespace_project = '' ;
+var interface_language = '' ;
+var interface_text = {} ;
+
+function _t ( k ) {
+	var ret = "<i>" + k + "</i>" ;
+	if ( typeof interface_text['en'][k] != 'undefined' ) ret = interface_text['en'][k] ;
+	if ( typeof interface_text[interface_language][k] != 'undefined' ) ret = interface_text[interface_language][k] ;
+	return ret ;
+}
+
+function applyParameters () {
+	// Radio
+	$.each ( params , function ( name , value ) {
+		
+		var m = name.match ( /^ns\[(\d+)\]$/ ) ;
+		if ( m != null ) {
+			namespaces_selected.push ( m[1]*1 ) ;
+			return ;
+		}
+		
+		$('input:radio[name="'+name+'"][value="'+value+'"]').prop('checked', true);
+	} ) ;
+}
+
+function setInterfaceLanguage ( l ) {
+	if ( interface_language == l ) return ;
+	interface_language = l ;
+	$.each ( interface_text['en'] , function ( k , v ) {
+		if ( typeof interface_text[l][k] != 'undefined' ) v = interface_text[l][k] ;
+		$('.l_'+k).html ( v ) ;
+	} ) ;
+}
+
+function loadInterface ( init_lang ) {
+	$.getJSON ( 'https://meta.wikimedia.org/w/api.php?action=parse&prop=wikitext&page=CatScan2/Interface&format=json&callback=?' , function ( d ) {
+		var wt = d.parse.wikitext['*'] ;
+		var rows = wt.split("\n") ;
+		var lang = '' ;
+		$.each ( rows , function ( k , v ) {
+			var m = v.match ( /^==\s*(.+?)\s*==$/ ) ;
+			if ( m != null ) {
+				lang = m[1].toLowerCase() ;
+				interface_text[lang] = {} ;
+				
+				var h = '<a class="dropdown-item" href="#">' + m[1] + '</a>' ;
+				$('#interface_languages').append ( h ) ;
+				return ;
+			}
+			m = v.match ( /^;(.+?):(.*)$/ ) ;
+			if ( m == null ) return ;
+			if ( m[1] == 'toolname' ) m[2] = 'PetScan' ; // HARDHACK FIXME
+			interface_text[lang][m[1]] = m[2] ;
+		} ) ;
+		
+		$('#interface_languages a.dropdown-item').click ( function () {
+			var o = $(this) ;
+			var lang = o.text().toLowerCase() ;
+			setInterfaceLanguage ( lang ) ;
+		} ) ;
+		
+		setInterfaceLanguage ( init_lang ) ;
+
+		loadNamespaces() ;
+		$('input[name="language"]').keyup ( loadNamespaces ) ;
+		$('input[name="project"]').keyup ( loadNamespaces ) ;
+		
+		applyParameters() ;
+	} ) ;
+}
+
+function loadNamespaces () {
+	if ( namespaces_loading ) return ;
+	var l = $('input[name="language"]').val() ;
+	if ( l.length < 2 ) return ;
+	var p = $('input[name="project"]').val() ;
+	var lp = l+'.'+p ;
+	if ( lp == last_namespace_project ) return ;
+	
+	var server = lp+'.org' ;
+	namespaces_loading = true ;
+	$.getJSON ( 'https://'+server+'/w/api.php?action=query&meta=siteinfo&siprop=namespaces&format=json&callback=?' , function ( d ) {
+		namespaces_loading = false ;
+		if ( typeof d == 'undefined' ) return ;
+		if ( typeof d.query == 'undefined' ) return ;
+		if ( typeof d.query.namespaces == 'undefined' ) return ;
+		last_namespaces = {} ;
+		max_namespace_id = 0 ;
+		$.each ( d.query.namespaces , function ( id , v ) {
+			id *= 1 ;
+			if ( id < 0 ) return ;
+			var title = v['*'] ;
+			if ( title == '' ) title = _t('namespace_0') ;
+			last_namespaces[id] = [ title , (v.canonical||title) ] ;
+			if ( id > max_namespace_id ) max_namespace_id = id ;
+		} ) ;
+		
+		last_namespace_project = lp ;
+		
+		var nsl = [] ;
+		function renderNS ( ns ) {
+			var h = "<div>" ;
+			if ( typeof last_namespaces[ns] != 'undefined' ) {
+				h += "<label" ;
+				if ( last_namespaces[ns][0] != last_namespaces[ns][1] ) h += " title='"+(last_namespaces[ns][1]||'')+"'" ;
+				h += "><input type='checkbox' value='1' ns='"+ns+"' name='ns["+ns+"]'" ;
+				if ( $.inArray ( ns , namespaces_selected ) >= 0 ) {
+					nsl.push ( ns ) ;
+					h += " checked" ;
+				}
+				h += "> " ;
+				h += last_namespaces[ns][0] ;
+				h += "</label>" ;
+			} else h += "&mdash;" ;
+			h += "</div>" ;
+			return h ;
+		}
+		
+//		var h = "<h3><span class='l_namespaces'>" + _t('namespaces') + "</span> " + lp + "</h3>" ;
+//		var h = "<div><tt>" + lp + "</tt></div>" ;
+		$(".current_wiki").text ( lp ) ;
+		h = "" ;
+		h += "<div class='smaller'>" ;
+		for ( var ns = 0 ; ns <= max_namespace_id ; ns += 2 ) {
+			if ( typeof last_namespaces[ns] == 'undefined' && typeof last_namespaces[ns+1] == 'undefined' ) continue ;
+			h += "<div class='ns-block'>" ;
+			h += renderNS ( ns ) ;
+			h += renderNS ( ns+1 ) ;
+			h += "</div>" ;
+		}
+		h += "</div>" ;
+		namespaces_selected = nsl ;
+		
+		$('#namespaces').html ( h ) ;
+		$('#namespaces input').change ( function () {
+			var o = $(this) ;
+			var ns = o.attr('ns') * 1 ;
+			namespaces_selected = jQuery.grep(namespaces_selected, function(value) { return value != ns; }); // Remove is present
+			if ( o.is(':checked') ) namespaces_selected.push ( ns ) ;
+		} ) ;
+		
+	} ) . always ( function () {
+		namespaces_loading = false ;
+	} ) ;
+	;
+}
+
+$(document).ready ( function () {
+	params = $.extend ( {} , default_params ) ;
+	loadInterface ( 'en' ) ;
+	$('input[name="language"]').focus() ;
+} ) ;
