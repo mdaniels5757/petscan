961cef997bc229b37f22469d19f8484139fcbd2e
Magnus Manske <magnusmanske@googlemail.com>
2016-04-05 09:44:38 +0000

    script to pull latest code, compile, and restart live site

---
+++ update_live_site.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get latest code
+git pull
+
+# Clean up old
+make clean
+
+# Build new server binary
+make -j4 server
+
+# Get restart code from config file
+code=`jq -r '.["restart-code"]' config.json`
+
+# Build restart URL
+url="https://petscan.wmflabs.org/restart?$code"
+
+# Restart server
+curl -s -o /dev/null url
+echo "Server restarted!"
