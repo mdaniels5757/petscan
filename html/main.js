7b3313a3b7821fff3009b93eac21b7fa804ad0e8
Magnus Manske <magnusmanske@googlemail.com>
2016-04-13 15:38:47 +0000

    fixed #13 and #23

--- html/main.js
+++ html/main.js
@@ -159,6 +159,10 @@ function setInterfaceLanguage ( l ) {
 	// Misc special updates
 	$('#query_length').text ( _t('query_length').replace('$1',$('#query_length').attr('sec')) ) ;
 	$('#num_results').text ( _t('num_results').replace('$1',$('#num_results').attr('num')) ) ;
+
+	// Permalink
+	var query = decodeURIComponent ( $('#querystring').text() ) ;
+	if ( query != '' ) setPermalink ( query ) ;
 	
 	$('#permalink a').each ( function () {
 		var a = $(this) ;
@@ -167,7 +171,6 @@ function setInterfaceLanguage ( l ) {
 		if ( h == h2 ) h2 = h + "&interface_language=" + l ;
 		a.attr ( { href : h2 } ) ;
 	} ) ;
-	
 	
 	return false ;
 }
