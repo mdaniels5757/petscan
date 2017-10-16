0c00ace28124b6ac86fbe4da195452e8db756cd6
Magnus Manske <magnusmanske@gmail.com>
2017-10-16 08:49:08 +0000

    switching back to analytics servers

--- wikidata_db.cpp
+++ wikidata_db.cpp
@@ -27,8 +27,8 @@ bool TWikidataDB::setHostDBFromWiki ( string wiki ) {
 	
 	if ( 0 ) { // Future special cases
 	} else {
-		_host = wiki+".web.db.svc.eqiad.wmflabs" ; // Fast ones
-//		_host = wiki+".analytics.db.svc.eqiad.wmflabs" ; // New, long-query server, use this if possible
+//		_host = wiki+".web.db.svc.eqiad.wmflabs" ; // Fast ones
+		_host = wiki+".analytics.db.svc.eqiad.wmflabs" ; // New, long-query server, use this if possible
 //		_host = wiki+".labsdb" ; // Old host, try not to use
 		_database = wiki+"_p" ;
 	}
